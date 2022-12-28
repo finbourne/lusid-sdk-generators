bbed7c7010a4b9f5dd8731bbd273d83ea739c1bc
Rizwan Saeed <rizwan.saeed@finbourne.com>
2022-12-28 14:19:58 +0000

    Revert "CTECH-600 fix python and c# config name param"
    This reverts commit 3d63108ee1d67f739278b49c0fd7c43f84b8030f.

--- csharp/generate/generate.sh
+++ csharp/generate/generate.sh
@@ -21,8 +21,10 @@ fi
 gen_root=$1
 output_folder=$2
 swagger_file=$output_folder/$3
-config_file_name=$4
+sdk_config=$4
 sdk_output_folder=$output_folder/sdk
+
+ignore_file_name=.openapi-generator-ignore
 
 if [[ -z $config_file_name || ! -f $gen_root/$config_file_name ]] ; then
     echo "[INFO] '$config_file_name' not found, using default config.json"
@@ -34,7 +36,6 @@ echo "[INFO] output folder   : $output_folder"
 echo "[INFO] swagger file    : $swagger_file"
 echo "[INFO] config file     : $config_file_name"
 
-ignore_file_name=.openapi-generator-ignore
 config_file=$gen_root/$config_file_name
 ignore_file=$output_folder/$ignore_file_name
 
@@ -42,7 +43,7 @@ app_name=$(cat $config_file | jq -r .packageName)
 
 #   remove all previously generated files
 shopt -s extglob 
-echo "[INFO] removing previous sdk:"
+echo "removing previous sdk:"
 rm -rf $sdk_output_folder/$app_name/!(Utilities|*.csproj)
 shopt -u extglob 
 
@@ -53,12 +54,12 @@ cp $ignore_file $sdk_output_folder
 # set versions
 sdk_version=$(cat $swagger_file | jq -r '.info.version')
 
-echo "[INFO] setting version to $sdk_version"
+echo "setting version to $sdk_version"
 
 cat $config_file | jq -r --arg SDK_VERSION "$sdk_version" '.packageVersion |= $SDK_VERSION' > temp && mv temp $config_file
 sed -i 's/<Version>.*<\/Version>/<Version>'$sdk_version'<\/Version>/g' $sdk_output_folder/$app_name/$app_name.csproj
 
-echo "[INFO] generating sdk"
+echo "generating sdk"
 
 #java -jar swagger-codegen-cli.jar swagger-codegen-cli help
 java -jar openapi-generator-cli.jar generate \
